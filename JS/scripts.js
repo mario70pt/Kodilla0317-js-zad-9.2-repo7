e32f48966fb5b94752f6c8245b952be00fceea0d
mario70pt <mario70pt@users.noreply.github.com>
2017-04-29 19:39:32 +0200

    first commit

---
+++ JS/scripts.js
@@ -0,0 +1,9 @@
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+var allNames = femaleNames.concat(maleNames)
+console.log(allNames);
+var newName = 'Marian';
+var newName = allNames.push('Marian');
+if (allNames.indexOf(newName) === -1) {
+    console.log(allNames);
+}
