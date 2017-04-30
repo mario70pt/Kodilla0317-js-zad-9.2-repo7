0be5da077e9888dbb6fbc20364877c059158a175
mario70pt <mario70pt@users.noreply.github.com>
2017-04-30 19:07:45 +0200

    korekta deklaracji i nazwy pliku na js

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,9 +1,9 @@
 var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
 var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
-var allNames = femaleNames.concat(maleNames)
+var allNames = femaleNames.concat(maleNames);
 console.log(allNames);
 var newName = 'Marian';
-if (allNames.indexOf(newName) === -1) {
-    var newName = allNames.push('Marian');
+if (allNames.indexOf('Marian') === -1) {
+    allNames[8] = 'Marian';
     console.log(allNames);
 }
